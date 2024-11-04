7fd96a1fd91506d7584c79d4e635bbe723a7c594
sarataha01 <sara56eng@gmail.com>
2024-11-04 17:04:48 +0200

    checkWinner modified

--- lib/services/game_provider.dart
+++ lib/services/game_provider.dart
@@ -37,39 +37,27 @@ class GameProvider extends ChangeNotifier {
 
   void checkWinner() {
     matchedIndexes.clear();
+    List<List<int>> winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
 
-    if (board[0] == board[1] && board[0] == board[2] && board[0] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([0, 1, 2]);
-    } else if (board[3] == board[4] && board[3] == board[5] && board[3] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([3, 4, 5]);
-    } else if (board[6] == board[7] && board[6] == board[8] && board[6] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([6, 7, 8]);
-    } else if (board[0] == board[3] && board[0] == board[6] && board[0] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([0, 3, 6]);
-    } else if (board[1] == board[4] && board[1] == board[7] && board[1] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([1, 4, 7]);
-    } else if (board[2] == board[5] && board[2] == board[8] && board[2] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([2, 5, 8]);
-    } else if (board[0] == board[4] && board[0] == board[8] && board[0] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([0, 4, 8]);
-    } else if (board[2] == board[4] && board[2] == board[6] && board[2] != '') {
-      result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
-      matchedIndexes.addAll([2, 4, 6]);
-    } else if (!board.contains('')) {
-      result = "It's a tie!";
+    for (var combination in winningCombinations) {
+      int a = combination[0];
+      int b = combination[1];
+      int c = combination[2];
+
+      if (board[a] == board[b] && board[a] == board[c] && board[a] != '') {
+        result = 'Player ${isXTurn ? 'O' : 'X'} wins!';
+        matchedIndexes.addAll([a, b, c]);
+        isXTurn ? oScore++ : xScore++;
+        return;
+      }
     }
 
-    if (result.contains('wins')) {
-      isXTurn ? oScore++ : xScore++;
-      debugPrint(result);
-      debugPrint("Matched indexes: $matchedIndexes");
+    if (!board.contains('')) {
+      result = "It's a tie!";
     }
   }
 }
